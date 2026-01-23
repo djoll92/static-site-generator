3ab61a26207f0340aa5a1ed3a5ad7f655b9efee1
djoll92 <djoll92@gmail.com>
2026-01-23 08:21:33 +0100

    Add LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -40,6 +40,23 @@ class TestHTMLNode(unittest.TestCase):
 		expected = f"HTMLNode(div, None, [{str(child1)}, {str(child2)}], None)"
 		self.assertEqual(expected, str(node))
 
+
+class TestLeafNode(unittest.TestCase):
+	def test_leaf_to_html_p(self):
+		node = LeafNode("p", "Hello, world!")
+		self.assertEqual(node.to_html(), "<p>Hello, world!</p>")		
+
+	def test_leaf_to_html_a(self):
+		node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+		self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+
+	def test_leaf_to_html_none_tag(self):
+		node = LeafNode(None, "Hello there")
+		self.assertEqual(node.to_html(), "Hello there")
+
+	def test_repr(self):
+		node = LeafNode("p", "Hello, world!")
+		self.assertEqual(str(node), "LeafNode(p, Hello, world!, None)")	
 
 if __name__ == "__main__":
 	unittest.main()
